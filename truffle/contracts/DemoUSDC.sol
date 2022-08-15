53bcb4a9a59136244e6c03b3134150b3c884b56e
Sagar Gopale <sagar090989@gmail.com>
2022-08-15 20:33:32 +0530

    Added buy function

---
+++ truffle/contracts/DemoUSDC.sol
@@ -0,0 +1,71 @@
+// SPDX-License-Identifier: MIT
+
+import "../node_modules/@openzeppelin/contracts/utils/Context.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../node_modules/@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Address.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "./Migrations.sol";
+
+/**
+ * Copyright (c) 2020 Coinbase, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+pragma solidity ^0.8.2;
+
+contract DemoUSDC is Ownable, ERC20 {   
+    uint8 private _decimals;
+
+    /**
+     * @dev Sets {decimals} to a value other than the default one of 18.
+     *
+     * WARNING: This function should only be called from the constructor. Most
+     * applications that interact with token contracts will not expect
+     * {decimals} to ever change, and may work incorrectly if it does.
+     */
+    function setupDecimals(uint8 decimals_) internal {
+        _decimals = decimals_;
+    }
+
+    constructor() public Ownable() ERC20("DemoUSDC", "USDC") {
+        setupDecimals(6);
+    }
+
+    function mint(address account, uint256 amount) external onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(address account, uint256 amount) external onlyOwner {
+        _burn(account, amount);
+    }
+
+    function decimals() override public view returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @notice Give coins caller
+     */
+    function getDUSDC() external {
+        _mint(msg.sender, 10e6);
+    }
+}
--- truffle/contracts/INRCToken.sol
+++ truffle/contracts/INRCToken.sol
@@ -1,14 +1,81 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.2;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "../node_modules/@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import "../node_modules/@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
+error InvalidExchangeCurrency();
+error LowExchangeCurrencyBalance();
+error MintToZeroAddress();
+error MintZeroQuantity();
+
+interface IExchangeCurrency {
+    function balanceOf(address account) external view returns (uint256);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function decimals() external view returns (uint8);
+}
 
 contract INRCToken is ERC20, Ownable, ReentrancyGuard {
+    IExchangeCurrency private ExchangeCurrency;
+    mapping(string => address) public exchangeCurrencies;
+
     constructor() ERC20("INRCToken", "INRC") {}
 
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
+
+    /** 
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * NOTE: Does not update the allowance if the current allowance
+     * is the maximum `uint256`.
+     *
+     * Requirements:
+     *
+     * - `from` and `to` cannot be the zero address.
+     * - `from` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``from``'s tokens of at least
+     * `amount`.
+     */
+    function addExchangeCurrency(string memory exchangeCurrencyName, address exchangeCurrencyAddress) public onlyOwner {
+        exchangeCurrencies[exchangeCurrencyName] = exchangeCurrencyAddress;
+    }
+
+    function buy(string memory exchangeCurrencyName, uint256 amount) public {
+        address receiver = msg.sender;
+        if (amount == 0) {
+            revert MintZeroQuantity();
+        }
+
+        if (msg.sender == address(0)) {
+            revert MintToZeroAddress();
+        }
+
+        uint256 amountWithoutDecimals = amount;
+        address exchangeCurrencyAddress = exchangeCurrencies[exchangeCurrencyName];
+        if(exchangeCurrencyAddress == address(0x0)) {
+            revert InvalidExchangeCurrency();
+        }
+        ExchangeCurrency = IExchangeCurrency(exchangeCurrencyAddress);
+        uint256 exchangeCurrencyBalance = ExchangeCurrency.balanceOf(receiver);
+        uint8 exchangeCurrencyDecimals = ExchangeCurrency.decimals();
+
+        // Adding transaction fee 0.5% to the amount
+        amount = amount * (10 ** exchangeCurrencyDecimals);
+        uint256 transactionFee = amount * 5 / 1000;
+        amount = amount + transactionFee;
+
+        if(exchangeCurrencyBalance < amount) {
+            revert LowExchangeCurrencyBalance();
+        }
+
+        ExchangeCurrency.transferFrom(receiver, address(this), amount);
+
+        uint256 tokensToMint = amountWithoutDecimals * 80 * (10 ** 18);
+        _mint(receiver, tokensToMint);
+    }
+
 }
